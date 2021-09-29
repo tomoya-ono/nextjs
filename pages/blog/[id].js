6eb4e183ddaeb73f24189e7e212b0001a86ddc64
tomoya ono <cdo01790@yahoo.co.jp>
2021-09-29 16:02:52 +0900

    static

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,13 +1,49 @@
 import { useRouter } from 'next/router'
 
-export default function Id() {
+
+export const getStaticPaths = async function () {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await res.json()
+    const allPaths = users.map(user => {
+        return {
+           params: {id: user.id.toString()}
+        }
+    })
+
+    const paths = allPaths.slice(0,5)
+
+    return {
+        paths,
+        fallback: true
+    }
+}
+
+
+export const getStaticProps = async function(context) {
+      const id = context.params.id
+      const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id)
+      const data = await res.json()
+
+      return {
+          props: { data },
+          revalidate: 5
+      }
+}
+
+export default function Id({ data }) {
     const router = useRouter()
-    const { id, foo } = router.query
+    if(router.isFallback){
+        return <div>loading</div>
+    }
     return (
         <>
-        <p>{id}</p>
-        <p>{foo}</p>
+        <p>{data.id}</p>
+        <p>{data.name}</p>
+        <p>{data.email}</p>
         </>  
     )
 }
+
+// getStaticPaths、getStaticProrpsはビルド時に一回だけ
+//全てのidでgetStaticProps, Idが実行される
 
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,25 @@
+import Link from "next/link";
 
-export default function index() {
-    return (
-        <div>
-            <p>index</p>
-        </div>
-    )
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: { users: data },
+  };
+};
+
+export default function Index({ users }) {
+
+return (
+    <div>
+      <h1>All Ninjas</h1>
+      {users.map(user => (
+        <Link href={`/blog/${user.id}`} key={user.id}>
+          <a>
+            <h3>{ user.name }</h3>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
 }
